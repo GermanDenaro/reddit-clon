b2dde734bdb14c6553e2a00b43ea753d6f5f2516
GermanDenaro <germandenaroc@gmail.com>
2023-03-02 16:11:52 -0300

    Commit

--- src/pages/r/[communityId]/Submit.tsx
+++ src/pages/r/[communityId]/Submit.tsx
@@ -1,6 +1,19 @@
+import PageContent from '@/src/components/Layout/PageContent';
+import NewPostForm from '@/src/components/Posts/NewPostForm';
+import { Box, Text } from '@chakra-ui/react';
 import React from 'react';
 
 const SubmitPostPage: React.FC = () => {
-  return <div>Have a good coding</div>;
+  return (
+    <PageContent>
+      <>
+        <Box p='14px 0px' borderBottom='1px solid' borderColor='white'>
+          <Text>Create a Post</Text>
+        </Box>
+        <NewPostForm />
+      </>
+      <>{/* <About /> */}</>
+    </PageContent>
+  );
 };
 export default SubmitPostPage;
